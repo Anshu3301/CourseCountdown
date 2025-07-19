4ac18c9d0a7a9c09610364420733b7e22c5f43bb
Anshu3301 <sainanshu40@gmail.com>
2025-07-20 02:22:40 +0530

    fix the days counter

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,13 @@ function App() {
       const difference = targetDate - now
 
       if (difference > 0) {
-        const months = Math.floor(difference / (1000 * 60 * 60 * 24 * 30.44))
-        const weeks = Math.floor((difference % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24 * 7))
-        const days = Math.floor((difference % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24))
+        let remainingDays = Math.floor(difference / (1000 * 60 * 60 * 24))
+        const months = Math.floor(remainingDays / 30.44)
+        remainingDays -= Math.floor(months * 30.44)
+        const weeks = Math.floor((remainingDays / 7))
+        remainingDays -= (weeks * 7)
+        const days = remainingDays
+
         const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
         const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
         const seconds = Math.floor((difference % (1000 * 60)) / 1000)
